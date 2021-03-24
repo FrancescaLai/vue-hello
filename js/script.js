bdfe1a30dad84e56ffc4d004281bbe15deb79bbd
FrancescaLai <francescalai1690@gmail.com>
2021-03-24 15:17:53 +0100

    Step 1 done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 1.Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// 2. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+var app = new Vue (
+  {
+    el: "#root",
+    data: {
+      message: "bibabu",
+  }
+});
